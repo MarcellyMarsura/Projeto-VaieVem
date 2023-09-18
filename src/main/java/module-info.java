37e10e585d85fcd833634b194eea95e4e175f2fe
Marcelly Marsura <marcellymarsura@gmail.com>
2023-09-18 02:26:44 -0300

    feat: Desenvolvimento telas Login e CadastroUsuario

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module br.edu.fesa.vaievem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens br.edu.fesa.vaievem to javafx.fxml;
     exports br.edu.fesa.vaievem;
