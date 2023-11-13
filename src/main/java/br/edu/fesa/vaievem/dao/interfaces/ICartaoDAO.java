91520e5c8408dc062510011374deb625a164b90c
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-11-12 21:12:46 -0300

    refactor: Reformulação interfaces DAO

--- src/main/java/br/edu/fesa/vaievem/dao/interfaces/ICartaoDAO.java
+++ src/main/java/br/edu/fesa/vaievem/dao/interfaces/ICartaoDAO.java
@@ -2,7 +2,12 @@
 package br.edu.fesa.vaievem.dao.interfaces;
 
 import br.edu.fesa.vaievem.models.Cartao;
+import java.util.List;
 
 public interface ICartaoDAO extends IGenericoDAO<Cartao> {
     
+    // Parâmetro descrição é opcional, pode vir null
+    List<Cartao> listarPorUsuario(long idUsuario, String descricao);
+    
+    List<Cartao> listarPorConta(long idContaBancaria);
 }
--- src/main/java/br/edu/fesa/vaievem/dao/interfaces/IContaBancariaDAO.java
+++ src/main/java/br/edu/fesa/vaievem/dao/interfaces/IContaBancariaDAO.java
@@ -2,7 +2,10 @@
 package br.edu.fesa.vaievem.dao.interfaces;
 
 import br.edu.fesa.vaievem.models.ContaBancaria;
+import java.util.List;
 
 public interface IContaBancariaDAO extends IGenericoDAO<ContaBancaria> {
-        
+    
+    // Parâmetro descrição é opcional, pode vir null
+    List<ContaBancaria> listarPorUsuario(long idUsuario, String descricao);
 }
